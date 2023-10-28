7ec849b0632b04c654c8be146b2b21715f543f16
Iqra Imran Syed <syiqra@hotmail.com>
2023-10-28 14:52:48 -0400

    database

--- server/data.sql
+++ server/data.sql
@@ -1,13 +1,31 @@
 CREATE DATABASE calendar;
 
 CREATE TABLE events (
-    id VARCHAR(255) PRIMARY KEY,
-    date VARCHAR(300),
-    user_email VARCHAR(255),
-    title VARCHAR(30),
+    id SERIAL PRIMARY KEY,
+    start_datetime TIMESTAMPTZ NOT NULL,
+    end_datetime TIMESTAMPTZ NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    location VARCHAR(100),
+    repeat_id INT REFERENCES repeat_options(id),
+    category_id INT REFERENCES categories(id),
+    user_id INT REFERENCES users(id)
+);
+
+
+CREATE TABLE repeat_options (
+    id SERIAL PRIMARY KEY,
+    option_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE users (
-    email VARCHAR(255) PRIMARY KEY,
-    hashed_password VARCHAR(255)
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
 );
